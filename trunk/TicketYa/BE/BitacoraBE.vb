124db6056fb9d08ec33372ca46dd7a0ff6a7eb07
javiermartingonzalez@gmail.com <javiermartingonzalez@gmail.com@2d4cb2db-67fd-1a03-1e81-77401ddb6152>
2012-08-28 03:54:54 +0000

    BE No funcionales

---
+++ trunk/TicketYa/BE/BitacoraBE.vb
@@ -0,0 +1,45 @@
+﻿Public Class BitacoraBE
+    Inherits PersistibleBE
+
+
+    Enum tipoError
+        Critico
+        ErrorTipico
+        Exito
+    End Enum
+
+
+    Private _usuario As UsuarioBE
+    Public Property usuario() As UsuarioBE
+        Get
+            Return _usuario
+        End Get
+        Set(ByVal value As UsuarioBE)
+            _usuario = value
+        End Set
+    End Property
+
+
+    Private _fecha As DateTime
+    Public Property fecha() As DateTime
+        Get
+            Return _fecha
+        End Get
+        Set(ByVal value As DateTime)
+            _fecha = value
+        End Set
+    End Property
+
+
+    Private _tipo As tipoError
+    Public Property tipo() As tipoError
+        Get
+            Return _tipo
+        End Get
+        Set(ByVal value As tipoError)
+            _tipo = value
+        End Set
+    End Property
+
+
+End Class
---
+++ trunk/TicketYa/BE/FamiliaBE.vb
@@ -0,0 +1,27 @@
+﻿Public Class FamiliaBE
+    Inherits PersistibleBE
+
+    Private _descripcion As String
+
+    Public Property descripcion() As String
+        Get
+            Return _descripcion
+        End Get
+        Set(ByVal value As String)
+            _descripcion = value
+        End Set
+    End Property
+
+
+    Private _permisos As List(Of PermisoBE)
+    Public Property permisos() As List(Of PermisoBE)
+        Get
+            Return _permisos
+        End Get
+        Set(ByVal value As List(Of PermisoBE))
+            _permisos = value
+        End Set
+    End Property
+
+
+End Class
