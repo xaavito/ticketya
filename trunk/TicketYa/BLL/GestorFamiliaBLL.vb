daf74125dfd37edcb745105f7e6c7f5b16ae5f29
javiermartingonzalez@gmail.com <javiermartingonzalez@gmail.com@2d4cb2db-67fd-1a03-1e81-77401ddb6152>
2012-08-29 03:15:54 +0000

    Nueva aproximacion a BLL no funcionales

---
+++ trunk/TicketYa/BLL/GestorFamiliaBLL.vb
@@ -0,0 +1,24 @@
+﻿Public Class GestorFamiliaBLL
+
+    Public Function crearFamilia(ByVal desc As String) As Boolean
+        Return Nothing
+    End Function
+
+    Public Function eliminarFamilia(ByVal fam As BE.FamiliaBE) As Boolean
+        Return Nothing
+    End Function
+
+    Public Function getPermisoFamilia(ByVal fam As BE.FamiliaBE) As List(Of BE.PermisoBE)
+        Return Nothing
+    End Function
+
+    Public Function listarFamilias(ByVal fam As String) As List(Of BE.FamiliaBE)
+        Return Nothing
+    End Function
+
+    Public Function modificarFamilia(ByVal fam As BE.FamiliaBE,
+                                     ByVal desc As String) As Boolean
+        Return Nothing
+    End Function
+
+End Class
---
+++ trunk/TicketYa/BLL/GestorPermisoBLL.vb
@@ -0,0 +1,21 @@
+﻿Public Class GestorPermisoBLL
+
+    Public Function borrarPermiso(ByVal desc As BE.PermisoBE) As Boolean
+        Return Nothing
+    End Function
+
+    Public Function buscarFamilia(ByVal desc As String) As List(Of BE.PermisoBE)
+        Return Nothing
+    End Function
+
+    Public Function crearPermiso(ByVal desc As String) As Boolean
+        Return Nothing
+    End Function
+
+    Public Function modificarFamilia(ByVal desc As String,
+                                     ByVal perm As BE.PermisoBE) As Boolean
+        Return Nothing
+    End Function
+
+
+End Class
