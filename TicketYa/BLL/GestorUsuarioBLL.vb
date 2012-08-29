f8f09516c9accb30ee38d0c4127fe8e57f534cda
javiermartingonzalez@gmail.com <javiermartingonzalez@gmail.com@2d4cb2db-67fd-1a03-1e81-77401ddb6152>
2012-08-29 03:15:54 +0000

    Nueva aproximacion a BLL no funcionales

---
+++ TicketYa/BLL/GestorUsuarioBLL.vb
@@ -0,0 +1,38 @@
+﻿Public Class GestorUsuarioBLL
+
+    Public Function buscarUsuario(ByVal usr As String, ByVal pass As String) As BE.UsuarioBE
+        Return Nothing
+    End Function
+
+    Public Function crearUsuario(ByVal usr As String,
+                                 ByVal pass As String,
+                                 ByVal nom As String,
+                                 ByVal ape As String,
+                                 ByVal mail As String,
+                                 ByVal dni As Long) As BE.UsuarioBE
+        Return Nothing
+    End Function
+
+    Public Function eliminarUsuario(ByVal usr As BE.UsuarioBE) As Boolean
+        Return Nothing
+    End Function
+
+    Public Function getPermisoUsuario(ByVal usr As BE.UsuarioBE) As List(Of BE.PermisoBE)
+        Return Nothing
+    End Function
+
+    Public Function crearUsuario(ByVal usr As String,
+                                 ByVal pass As String,
+                                 ByVal nom As String,
+                                 ByVal ape As String,
+                                 ByVal mail As String,
+                                 ByVal dni As Long,
+                                 ByVal usuario As BE.UsuarioBE) As Boolean
+        Return Nothing
+    End Function
+
+    Public Function obtenerEstadoUsuario(ByVal usr As BE.UsuarioBE) As Integer
+        Return Nothing
+    End Function
+
+End Class
