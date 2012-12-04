64bede8095e5c1b10196a69584dddb21b0b2deec
javiermartingonzalez@gmail.com <javiermartingonzalez@gmail.com@2d4cb2db-67fd-1a03-1e81-77401ddb6152>
2012-12-04 03:16:34 +0000

    Promociones y buscar ventas

--- TicketYa/BE/PromocionBE.vb
+++ TicketYa/BE/PromocionBE.vb
@@ -13,4 +13,48 @@
     End Property
 
 
+    Private _tipoDescuento As String
+    Public Property tipoDescuento() As String
+        Get
+            Return _tipoDescuento
+        End Get
+        Set(ByVal value As String)
+            _tipoDescuento = value
+        End Set
+    End Property
+
+
+    Private _descuento As Decimal
+    Public Property descuento() As Decimal
+        Get
+            Return _descuento
+        End Get
+        Set(ByVal value As Decimal)
+            _descuento = value
+        End Set
+    End Property
+
+
+    Private _desde As Date
+    Public Property desde() As Date
+        Get
+            Return _desde
+        End Get
+        Set(ByVal value As Date)
+            _desde = value
+        End Set
+    End Property
+
+
+    Private _hasta As Date
+    Public Property hasta() As Date
+        Get
+            Return _hasta
+        End Get
+        Set(ByVal value As Date)
+            _hasta = value
+        End Set
+    End Property
+
+
 End Class
